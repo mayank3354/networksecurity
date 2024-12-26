8aea8f4937230fc2259beb5105a319b2d93195d3
mayank3354 <mayankkhs@gmail.com>
2024-12-26 10:55:08 +0530

    Data ingestion completed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -11,5 +11,7 @@ class NetworkSecurityException(Exception):
 
 
     def __str__(self):
-        return f"Error: {self.error_message} at line number {self.lineno} in file {self.file_name}"
-        
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+               self.file_name, self.lineno, str(self.error_message) 
+        )
+            
